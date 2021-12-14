c14de1e669f5cd5069ce1f05ab3653dfeb3865ac
wjdwl002 <wjdwl001@khu.ac.kr>
2021-12-14 19:36:15 +0900

    12/14

--- Gulp 90분 마스터하기/gulpfile.babel.js
+++ Gulp 90분 마스터하기/gulpfile.babel.js
@@ -1,2 +1,18 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
 //const gulp = require("gulp");
+
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest: "build"
+    }
+}
+
+export const pug = () => 
+    gulp
+    .src(routes.pug.src)
+    .pipe(gpug())
+    .pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([pug]);
